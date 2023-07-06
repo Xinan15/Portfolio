a781f31ffb2f4ba26f5cc316a356c5579fa15329
Xinan <youkingan11@gmail.com>
2023-07-06 17:47:42 +0100

    add down arrow

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
 }
+
+
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 100
+      }, 400, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
