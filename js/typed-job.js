c60678dddf7510122a3dbb9268b2c5a68c05af92
Xinan <youkingan11@gmail.com>
2023-07-16 16:56:29 +0100

    add data analysis, and change the job title

--- js/typed-job.js
+++ js/typed-job.js
@@ -1,12 +1,12 @@
 var options = {
-  strings: ["Web　Developer"],
+  strings: ["Junior Software Developer"],
   startDelay: 2000,
   typeSpeed: 60,
   onComplete: (self) => {
     window.setTimeout(() => {
       document.querySelector(".typed-cursor").style.display = "none";
       let job = document.querySelector("#typed-job");
-      job.innerHTML = "Web　Developer";
+      job.innerHTML = "Junior Software Developer";
       job.classList.add(
         "job-after-typing",
         "animate__animated",
